ba82cd39d2bcf807bfdd98204c98038a3b594a9d
hues0s <hector.ullate@gmail.com>
2022-06-07 19:53:22 +0200

    - Implemented Coin row view so that we can use it to display a list of coins in our app - Implemented Double extension, to help format Double numbers into strings

--- iCrypto/Core/Home/View/CoinRowView.swift
+++ iCrypto/Core/Home/View/CoinRowView.swift
@@ -10,9 +10,37 @@ import SwiftUI
 struct CoinRowView: View {
     
     let coin: CoinModel
+    let showHoldingsColumn: Bool
     
     var body: some View {
         HStack {
+            leftColumn
+            Spacer()
+            if showHoldingsColumn {
+                centerColumn
+            }
+            rightColumn
+        }
+        .font(.subheadline)
+    }
+}
+
+struct CoinRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            CoinRowView(coin: dev.coin, showHoldingsColumn: true)
+                .previewLayout(.sizeThatFits)
+            CoinRowView(coin: dev.coin, showHoldingsColumn: true)
+                .previewLayout(.sizeThatFits)
+                .preferredColorScheme(.dark)
+        }
+    }
+}
+
+extension CoinRowView {
+    
+    private var leftColumn: some View {
+        HStack(spacing: 0) {
             Text("\(coin.rank)")
                 .font(.caption)
                 .foregroundColor(Color.theme.secondaryText)
@@ -23,20 +51,26 @@ struct CoinRowView: View {
                 .font(.headline)
                 .padding(.leading, 6)
                 .foregroundColor(Color.theme.accent)
-            Spacer()
-            VStack(alignment: .trailing) {
-                Text("\(coin.currentPrice)")
-                    .bold()
-                    .foregroundColor(Color.theme.accent)
-                Text("\(coin.priceChangePercentage24H ?? 0)%")
-                    .foregroundColor((coin.priceChangePercentage24H ?? 0) >= 0 ? Color.theme.green : Color.theme.red)
-            }
         }
     }
-}
-
-struct CoinRowView_Previews: PreviewProvider {
-    static var previews: some View {
-        CoinRowView(coin: dev.coin)
+    
+    private var centerColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentHoldingsValue.asCurrencyWith2Decimals())
+                .bold()
+            Text((coin.currentHoldings ?? 0).asNumberString())
+        }
     }
+    
+    private var rightColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentPrice.asCurrencyWith6Decimals())
+                .bold()
+                .foregroundColor(Color.theme.accent)
+            Text(coin.priceChangePercentage24H?.asPercentString() ?? "")
+                .foregroundColor((coin.priceChangePercentage24H ?? 0) >= 0 ? Color.theme.green : Color.theme.red)
+        }
+        .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+    }
+    
 }
